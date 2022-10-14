7e6de09938ed9e4a32b119188d9f6b3ccf07a1fb
coderchemivan <igomezs1502@comunidad.unam.mx>
2022-10-13 19:22:12 -0500

    "load info to json with crawling process"

--- extraccion_peliculas.py
+++ extraccion_peliculas.py
@@ -12,20 +12,21 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 import os
+import json
 
 
-buscar_repetidos = False
+
+
 archivo = input('Ingrese el nombre del archivo: ')
-num_archivo = re.findall('(\d+)',archivo)
 archivo = 'files/{archivo}.json'.format(archivo = archivo)
-existencia_archivo = os.path.exists(archivo)
-if existencia_archivo == True:
+contador_descargas = 0
+verificar_existencia_archivo = input("¿Desea verificar la existencia de un archivo? (s/n): ")
+if verificar_existencia_archivo == "s":
     df = pd.read_json(archivo).astype(str)
     df = df.iloc[:, 0]
     df = df.apply(lambda x: x[2:len(x)-2])
-    buscar_repetidos = True
-else:
-    print('El archivo no existe')
+    #print(df)
+
 
 
 class Opinion(Item):
@@ -60,7 +61,7 @@ class Imdb (CrawlSpider):
 
     urls = [url.strip() for url in urls]
 
-    start_urls = [urls[0]]
+    start_urls = [urls[2]]
 
     download_delay = 1
 
@@ -82,122 +83,151 @@ class Imdb (CrawlSpider):
     
 
     def parse_titles(self, response):
-
-        
-    
         sel  = Selector(response)
         item = ItemLoader(Opinion(),sel)
-
+ 
         '''ID'''
         links = sel.xpath("//*[@class='ipc-metadata-list-item__icon-link']/@href").getall()
-        id_imdb = re.findall('/title/(tt\d+)/',''.join(links))[0]
-        item.add_value('id_imdb', id_imdb)   
-
-        if buscar_repetidos == True:
-            if id_imdb in df.values:
-                print('La pelicula ya existe')
-                print('__________________________--','\n')
-            else:
-                '''Titulo'''
-                try:
-                    titulo = sel.xpath('//*[@hero-title-block__original-title="hero-title-block__original-title"]/text()')[0].get()
-                    
-                except:
-                    titulo = sel.xpath('//*[@data-testid="hero-title-block__title"]/text()')[0].get()
-                
-                
-                item.add_value('title', titulo)
-                
-                '''genre'''
-                try:
-                    genres = sel.xpath("//*[@class='ipc-chip__text']/text()")
-                    for genero in genres:
-                        item.add_value('genre',genero.get())
-                except:
-                    item.add_value('genre','ND')
-
-
-                '''rating'''
-                try:
-                    rating = sel.xpath("//*[@class='sc-7ab21ed2-1 jGRxWM']/text()")[1].get()
-                    item.add_value('rating',rating)
-                except:
-                    item.add_value('rating', 'ND')
-
-
-
-                '''year'''
-                try:
-                    year = sel.xpath("//*[@class='sc-8c396aa2-2 itZqyK']/text()")[0].get()
-                    item.add_value('year',year)
-                except:
-                    item.add_value('year','ND')
-
-
-                '''durcion'''
-                try:
-                    
-                    duracion = sel.xpath("//*[@class='sc-80d4314-2 iJtmbR']/ul/li[3]/text()").getall()
-                    if duracion == []:
-                        duracion = sel.xpath("//*[@class='sc-80d4314-2 iJtmbR']/ul/li[2]/text()").getall()
-                    duracion = ''.join(duracion)
-                    item.add_value('duracion',duracion)
-                except:
-                    item.add_value('duracion','ND')
-
-
-                '''estrenada'''
-                try:
-                    estreno = sel.xpath("//*[@data-testid='tm-box-up-date']/text()")[0].get()
-                    item.add_value('estreno',estreno)
-                except:
-                    item.add_value('estreno',0)
-                        
-                '''budget'''
-                try:
-                    budget = sel.xpath("//*[@class='ipc-metadata-list__item sc-6d4f3f8c-2 fJEELB']/div/ul/li/span/text()")[0].get()
-                    item.add_value('budget',budget)
-                except:
-                    item.add_value('budget', 'ND')
-
-
-                '''recaudacion'''
-                try:
-                    recaudacion = sel.xpath("//*[@class='ipc-metadata-list__item sc-6d4f3f8c-2 fJEELB']/div/ul/li/span/text()")[4].get()
-                    item.add_value('boxOffice_collection',recaudacion)
-                except:
-                    item.add_value('boxOffice_collection', 'ND')
-
-                #
-                '''cast'''
-                try:
-                    cast = sel.xpath("//*[@data-testid='title-cast-item__actor']/text()")
-                    for actor in cast:
-                        item.add_value('cast', actor.get())
-                except:
-                    item.add_value('cast', 'ND')
-
-                '''personajes'''
-                try:
-                    chars = sel.xpath("//*[@data-testid='cast-item-characters-link']/span/text()")
-                    for char in chars:
-                        item.add_value('personajes', char.get())
-                except:
-                    item.add_value('personajes', 'ND')
+        id_imdb = re.findall('/title/(tt\d{7,11})/',''.join(links))[0]
+        item.add_value('id_imdb', id_imdb)      
+        
+        if verificar_existencia_archivo == "s" and id_imdb in df.values:
+            print('La película ya se encuentra en el archivo')        
+        else:
             
-                yield item.load_item() 
+            try:
+                titulo = sel.xpath('//*[@hero-title-block__original-title="hero-title-block__original-title"]/text()')[0].get()
+                
+            except:
+                titulo = sel.xpath('//*[@data-testid="hero-title-block__title"]/text()')[0].get()
 
 
-            def parse_cast(self, response):pass
+            '''Titulo'''
+            #item.add_value('title', titulo)
+            
+            
+
+            '''genre'''
+            try:
+                genres = sel.xpath("//*[@class='ipc-chip__text']/text()")
+                generos = [genero.get() for genero in genres]
+            #    for genero in genres:
+            #        item.add_value('genre',genero.get())
+            except:
+                generos = 'ND'
+                item.add_value('genre','ND')
+
+
+            '''rating'''
+            try:
+                rating = sel.xpath("//*[@class='sc-7ab21ed2-1 jGRxWM']/text()")[1].get()
+            #    item.add_value('rating',rating)
+            except:
+                rating = 'ND'
+            #    item.add_value('rating', 'ND')
 
 
 
+            '''year'''
+            try:
+                year = sel.xpath("//*[@class='sc-8c396aa2-2 itZqyK']/text()")[0].get()
+            #    item.add_value('year',year)
+            except:
+                year = 'ND'
+            #    item.add_value('year','ND')
 
 
-# if __name__ == '__main__':
-#     process = CrawlerProcess()
-#     process.crawl(Imdb)
-#     process.start()
+            '''durcion'''
+            try:
+                
+                duracion = sel.xpath("//*[@class='sc-80d4314-2 iJtmbR']/ul/li[3]/text()").getall()
+                if duracion == []:
+                    duracion = sel.xpath("//*[@class='sc-80d4314-2 iJtmbR']/ul/li[2]/text()").getall()
+                duracion = ''.join(duracion)
+            #    item.add_value('duracion',duracion)
+            except:
+                duracion = 'ND'
+            #    item.add_value('duracion','ND')
 
-#scrapy runspider extraccion_peliculas.py -o files/titulos1.json -t json
+
+            '''estrenada'''
+            try:
+                estreno = sel.xpath("//*[@data-testid='tm-box-up-date']/text()")[0].get()
+            #    item.add_value('estreno',estreno)
+            except:
+                estreno = 'ND'
+            #    item.add_value('estreno',0)
+                    
+            '''budget'''
+            try:
+                budget = sel.xpath("//*[@class='ipc-metadata-list__item sc-6d4f3f8c-2 fJEELB']/div/ul/li/span/text()")[0].get()
+            #    item.add_value('budget',budget)
+            except:
+                budget = 'ND'
+            #    item.add_value('budget', 'ND')
+
+
+            '''recaudacion'''
+            try:
+                recaudacion = sel.xpath("//*[@class='ipc-metadata-list__item sc-6d4f3f8c-2 fJEELB']/div/ul/li/span/text()")[4].get()
+            #    item.add_value('boxOffice_collection',recaudacion)
+            except:
+                recaudacion = 'ND'
+            #    item.add_value('boxOffice_collection', 'ND')
+
+            #
+            '''cast'''
+            try:
+                cast = sel.xpath("//*[@data-testid='title-cast-item__actor']/text()")
+                casts = [actor.get() for actor in cast]
+            #    for actor in cast:
+            #        item.add_value('cast', actor.get())
+            except:
+                casts = 'ND'
+            #    item.add_value('cast', 'ND')
+
+            '''personajes'''
+            try:
+                chars = sel.xpath("//*[@data-testid='cast-item-characters-link']/span/text()")
+                characters = [char.get() for char in chars]
+            #    for char in chars:
+            #        item.add_value('personajes', char.get())
+            except:
+                characters = 'ND'
+            #    item.add_value('personajes', 'ND')
+
+            items = {
+                'id_imdb':id_imdb,
+                'title':titulo,
+                'genre':generos,
+                'rating':rating,
+                'year':year,
+                'duracion':duracion,
+                'estreno':estreno,
+                'budget':budget,
+                'boxOffice_collection':recaudacion,
+                'cast':casts,
+                'personajes':characters
+            }
+            #agregando el diccionario al archivo json
+            # 1. Read file contents
+            with open(archivo, "r") as file:
+                data = json.load(file)
+            # 2. Update json object
+            data.append(items)
+            # 3. Write json file
+            with open(archivo, "w") as file:
+                json.dump(data, file)
+
+            
+            #print('Descargando película: ',contador_descargas)
+            #contador_descargas += 1
+            #yield item.load_item() 
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(Imdb)
+    process.start()
+
+#scrapy runspider extraccion_peliculas.py -o files/titulos2.json -t json
 
