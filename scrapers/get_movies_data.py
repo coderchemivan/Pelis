eef9af402479543ae56a51732589a508cce03871
Ivan Gomez <igomezs1502@comunidad.unam.mx>
2023-03-19 19:12:29 -0600

    IMDB Script start

--- scrapers/get_movies_data.py
+++ scrapers/get_movies_data.py
@@ -14,6 +14,9 @@ import pandas as pd
 from time import sleep
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
+from datetime import timedelta
+import random
 
 #from get_user_movies import MongoDB_admin
 
@@ -24,7 +27,12 @@ class IMDB (CrawlSpider):
     name = "Imdb titles"
     custom_settings = {
     'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36',
-    'CLOSESPIDER_PAGECOUNT': 30000         
+    'CLOSESPIDER_PAGECOUNT': 30000,    
+    'FEEDS' :  {
+    'files/mis_pelis_data.json' : {
+        'format': 'json'
+            }
+        }        
     }
 
     allowed_domains = ['imdb.com']
@@ -33,11 +41,12 @@ class IMDB (CrawlSpider):
     #generar una lista de urls con los titulos de las peliculas del archivo files/titulos.txt
 
 
-    with open('files/titulos1.txt','r') as f:
+    with open('files/titulos.txt','r') as f:
         urls = f.readlines()
         start_urls = [f'https://www.google.com/search?q={url.strip()} imdb' for url in urls]
  
-    download_delay = 1
+    
+    download_delay = 3
 
     rules = (
         Rule(#Detalle de películas
@@ -47,144 +56,161 @@ class IMDB (CrawlSpider):
             ), follow=True,callback='parse_titles'),
         )
     
-    
-    
-
     def parse_titles(self, response):
-
-    
         sel  = Selector(response)
+
+        movie_description = sel.xpath('//*[@class="sc-b5e8e7ce-0 dZsEkQ"]')
+        print(len(movie_description),'________________________________________________')
+        
+
         titulo = ''
         try:
-            titulo = sel.xpath("//div[contains(@data-testid,'original-title')]/text()")[0].get()
+            titulo = sel.xpath('//*[@data-testid="hero-title-block__original-title"]/text()')[0].get()
             
         except:
-            titulo = sel.xpath("//h1[contains(@data-testid,'title-block__title')]/text()")[0].get()
+            try:
+                titulo = sel.xpath('//*[@data-testid="hero-title-block__title"]/text()')[0].get()
+            except:
+                "poner el titulo de la url"
+                titulo = response.url.split('/')[-2]
+        titulo = titulo.replace('Original title: ','').strip()
             
-        
-        print(titulo,"_________________________________________________________________________________________")
-    #     #print(titulo)
-    #     #if titulo not in df.values:
+        '''genre'''
+        try:
+            genres = sel.xpath("//*[@class='ipc-chip__text' and not(contains(text(),'Back to top'))]/text()")
+            genres_list = [genre.get() for genre in genres]  
+        except:
+            genres = "ND"
 
-    #     '''Titulo'''
-        
-        
-
-    #     '''genre'''
-    #     try:
-    #         genres = sel.xpath("//*[@class='ipc-chip__text']/text()")
-    #         for genero in genres:
-    #             pass
-                
-    #     except:
-    #         genres = "ND"
-
-    #     '''rating'''
-    #     try:
-    #         rating = sel.xpath("//*[@class='sc-7ab21ed2-1 jGRxWM']/text()")[1].get()
+        '''rating'''
+        try:
+            rating = sel.xpath("//*[@class='sc-e457ee34-1 gvYTvP']/text()")[0].get()
             
-    #     except:
-    #         rating = "ND"
+        except:
+            rating = "ND"
 
-
-
-    #     '''year'''
-    #     try:
-    #         year = sel.xpath("//*[@class='sc-8c396aa2-2 itZqyK']/text()")[0].get()
+        '''year'''
+        try:
+            year = sel.xpath("//ul[@data-testid='hero-title-block__metadata']//span/text()")[0].get()
             
-    #     except:
-    #         year = "ND"
+        except:
+            year = "ND"
 
 
-    #     '''durcion'''
-    #     try:
+        '''durcion'''
+        try:
             
-    #         duracion = sel.xpath("//*[@class='sc-80d4314-2 iJtmbR']/ul/li[3]/text()").getall()
-    #         if duracion == []:
-    #             duracion = sel.xpath("//*[@class='sc-80d4314-2 iJtmbR']/ul/li[2]/text()").getall()
-    #         duracion = ''.join(duracion)
-    #     except:
-    #         duracion = "ND"
+            duracion = sel.xpath("//li[@data-testid='title-techspec_runtime']/div/text()").getall()
+            duracion = ''.join(duracion)
+            duracion = timedelta(hours=int(duracion.split()[0]), minutes=int(duracion.split()[2]))
+            duracion = duracion.__str__().split(':')[:-1]
+            duracion = ':'.join(duracion)
+        except:
+             duracion = "ND"
 
 
-    #     '''estrenada'''
-    #     try:
-    #         estreno = sel.xpath("//*[@data-testid='tm-box-up-date']/text()")[0].get()
-    #     except:
-    #        estreno = "ND"
+        '''budget'''
+        try:
+            budget = sel.xpath("//li[@data-testid='title-boxoffice-budget']/div//text()")[0].get()
+        except:
+            budget = "ND"
 
 
-    #     '''director'''
-    #         #//*[@class='ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content base'][1] me va a dar el director o guión
-    #     posibles_contenedores = sel.xpath("//*[@class='ipc-metadata-list__item']")
-    #     director_count = 0
-    #     for i in range(len(posibles_contenedores)):
-    #         direccion = posibles_contenedores[i].xpath('.//span/text()').get()
-    #         if direccion =="Director":
-    #             director_count+=1
-    #             if director_count ==3:
-    #                 directores_contenedor = sel.xpath("//*[@class='ipc-metadata-list__item']")[i]
-    #                 directores = directores_contenedor.xpath(".//div/ul/li/a/text()")
-    #                 for director in directores:
-    #                     pass
-    #         continue
-
-    #     '''plot'''
-        
-    #     plot_ = sel.xpath("//*[@class='sc-132205f7-0 bJEfgD']/div/div/div/text()")
-    #     print(plot_,'_______________________________________________________________________')
-
-    #     '''reparto'''
-    #     try:
-    #         reparto = sel.xpath("//*[@class='ipc-metadata-list-item__icon-link']/@href").getall()
-    #         posibles_links_de_reparto = re.findall('(/title/tt\d{7,11}/fullcredits/\?ref_=tt_cl_sm)',''.join(reparto))
-    #         link_reparto= 'https://www.imdb.com/'+posibles_links_de_reparto[0]
-    #         r = requests.get(link_reparto)
-    #         soup = BeautifulSoup(r.text, 'html.parser')
-    #         writing = soup.find_all('table', attrs={'class':'simpleTable simpleCreditsTable'})
-    #         writers = writing[1].find_all('a')
-    #         credit_papel = writing[1].find_all('td',attrs={'class':'credit'})
-    #         writing_credits = {}
-    #         c =0	
-    #         for writer in writers:
-    #             writing_credits[credit_papel[c].text.strip().replace('(','').replace(')','').replace('&','')] = writer.text.strip()
-    #             c+=1
-    #         print(writing_credits)
-    #     except:
-    #         guide = "ND"
-                
-    #     '''budget'''
-    #     try:
-    #         budget = sel.xpath("//*[@class='ipc-metadata-list__item sc-6d4f3f8c-2 fJEELB']/div/ul/li/span/text()")[0].get()
-    #     except:
-    #         budget = "ND"
+        '''recaudacion'''
+        try:
+            recaudacion = sel.xpath("//li[@data-testid='title-boxoffice-cumulativeworldwidegross']/div//text()")[0].get()
+        except:
+            recaudacion = "ND"
 
 
-    #     '''recaudacion'''
-    #     try:
-    #         recaudacion = sel.xpath("//*[@class='ipc-metadata-list__item sc-6d4f3f8c-2 fJEELB']/div/ul/li/span/text()")[4].get()
-    #     except:
-    #         recaudacion = "ND"
+        '''cast'''
+        try:
+            cast = sel.xpath("//*[@data-testid='title-cast-item__actor']/text()")
+            cast_list = [actor.get() for actor in cast]
+        except:
+            cast_list = "ND"
 
-    #     #
-    #     '''cast'''
-    #     try:
-    #         cast = sel.xpath("//*[@data-testid='title-cast-item__actor']/text()")
-    #         for actor in cast:
-    #             pass
-    #     except:
-    #        acto = "ND"
+        '''director'''
+        director = ''
+        try:
+            top_credits_container = sel.xpath("//*[@data-testid='title-pc-principal-credit']")
+            for index, credit in enumerate(top_credits_container):
+                texts = credit.xpath(".//text()").getall()
+                if "Director" in texts or "Directors" in texts:
+                    director = credit.xpath("div/ul/li/a/text()").getall()
+                    break
+        except Exception as e:
+            director = "ND"
 
-    #     '''personajes'''
-    #     try:
-    #         chars = sel.xpath("//*[@data-testid='cast-item-characters-link']/span/text()")
-    #         for char in chars:
-    #             pass
-    #     except:
-    #         chars = "ND"
+        '''escritores'''
+        writer = ''
+        try:
+            top_credits_container = sel.xpath("//*[@data-testid='title-pc-principal-credit']")
+            for index, credit in enumerate(top_credits_container):
+                texts = credit.xpath(".//text()").getall()
+                if "Writer" in texts or "Writers" in texts:
+                    writer = credit.xpath("div/ul/li/a/text()").getall()
+                    break
+        except Exception as e:
+            writer = "ND"
 
-    # def parse_cast(self, response):pass
+        '''production comopanies'''
+        production_companies = ''
+        try:
+            production_companies = sel.xpath("//*[@data-testid='title-details-companies']//li/a/text()").getall()
+        except:
+            production_companies = "ND"
 
+        '''languages'''
+        languages = ''
+        try:
+            languages = sel.xpath("//*[@data-testid='title-details-languages']//li/a/text()").getall()
+        except:
+            languages = "ND"        
+
+        '''country'''
+        country = ''
+        try:
+            country = sel.xpath("//*[@data-testid='title-details-origin']//li/a/text()").getall()
+        except:
+            country = "ND"
+
+        '''release date'''
+        release_date = ''
+        try:
+            release_date_country_original = sel.xpath("//*[@data-testid='title-details-releasedate']//li/a/text()")[0].get()
+            release_date_country = release_date_country_original.split(" ")
+            release_date = release_date_country[1] + " " + release_date_country[0] + " " + release_date_country[2]
+            release_date = datetime.strptime(release_date, "%d, %B %Y")
+            release_date = release_date.strftime("%d-%m-%Y")         
+
+            regex_pais = r"\((.*?)\)"
+            match = re.search(regex_pais, release_date_country_original)
+            pais_estreno = match.group(1)
+            
+
+        except Exception as e:
+            release_date = "ND"
+            pais_estreno = "ND"
+
+
+        yield {
+                'titulo': titulo,
+                'generos': genres_list,
+                'rating': rating,
+                'año': year,
+                'duracion': duracion,
+                'budget': budget,
+                'recaudacion_mundial': recaudacion,
+                'cast': cast_list,
+                'director': director,
+                'escritor': writer,
+                'casas_productoras': production_companies,
+                'idiomas': languages,
+                'países de origen': country,
+                'fecha_de_estreno': release_date,
+                'pais_estreno': pais_estreno
+        }
 
 #scrapy runspider extraccion_peliculas.py -o files/titulos1.json -t json
 
@@ -211,7 +237,3 @@ get_user_movies()
 #         f.write(movie['titulo']+' '+movie['año']+'\n')
         
        
-# with open('files/titulos.txt','r') as f:
-#     urls = f.readlines()
-#     for url in urls:
-#         print(url)
--- scrapers/prueba.py
+++
@@ -1,7 +0,0 @@
-
-
-
-
-with open('files/titulos.txt','r') as f:
-    urls = f.readlines()
-    urls = [f'https://www.google.com/search?q={url.strip()} imdb' for url in urls]
